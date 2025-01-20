61d8266a5a09c98d76e325fd32c02eea8a71d699
Fábio Mendes <fabiobends@gmail.com>
2025-01-19 21:13:42 -0300

    feat(database): add `Exec` function and remove fatal logs

--- internal/utils/database.go
+++ internal/utils/database.go
@@ -12,7 +12,7 @@ func newConnection() *pgx.Conn {
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 
 	if err != nil {
-		log.Fatalf("Unable to connect to database: %v", err)
+		log.Printf("Unable to connect to database: %v", err)
 	}
 
 	return conn
@@ -23,17 +23,17 @@ func QueryItem[T any](query string, params ...any) T {
 
 	rows, err := conn.Query(context.Background(), query, params...)
 
+	defer conn.Close(context.Background())
+
 	if err != nil {
-		log.Fatalf("Unable to query rows: %v", err)
+		log.Printf("Unable to query rows: %v", err)
 	}
 
 	item, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByNameLax[T])
 
 	if err != nil {
-		log.Fatalf("Unable to collect row: %v", err)
+		log.Printf("Unable to collect row: %v", err)
 	}
-
-	conn.Close(context.Background())
 
 	return item
 }
@@ -43,17 +43,34 @@ func QueryList[T any](query string, params ...any) []T {
 
 	rows, err := conn.Query(context.Background(), query, params...)
 
+	defer conn.Close(context.Background())
+
 	if err != nil {
-		log.Fatalf("Unable to query rows: %v", err)
+		log.Printf("Unable to query rows: %v", err)
+		return []T{}
 	}
 
 	items, err := pgx.CollectRows(rows, pgx.RowToStructByNameLax[T])
 
 	if err != nil {
-		log.Fatalf("Unable to query rows: %v", err)
+		log.Printf("Unable to query rows: %v", err)
+		return []T{}
 	}
 
-	conn.Close(context.Background())
-
 	return items
+}
+
+func Exec(query string, params ...any) error {
+	conn := newConnection()
+
+	_, err := conn.Exec(context.Background(), query, params...)
+
+	defer conn.Close(context.Background())
+
+	if err != nil {
+		log.Printf("Unable to execute query: %v", err)
+		return err
+	}
+
+	return nil
 }
